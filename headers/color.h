6fdbc730277ae7f94a66b88795d7f55237f8dae2
sakkshm <saksham.saxena404@gmail.com>
2025-01-18 19:33:00 +0530

    Consolidated code and added hittable_list &  Anti aliasing

--- headers/color.h
+++ headers/color.h
@@ -1,10 +1,22 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+
 #include "vec3.h"
 #include <iostream>
+#include "interval.h"
 
 using color = vec3;
+
+double clamp(double x)  {
+    double min = 0.000;
+    double max = 0.999;
+
+    if(x < min) return min;
+    if(x > max) return max;
+
+    return x;
+}
 
 void write_color(std::ostream& out, const color& pixel_color){
     auto r = pixel_color.x();
@@ -12,9 +24,9 @@ void write_color(std::ostream& out, const color& pixel_color){
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255]
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    int rbyte = int(256 * clamp(r));
+    int gbyte = int(256 * clamp(g));
+    int bbyte = int(256 * clamp(b));
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
--- headers/vec3.h
+++ headers/vec3.h
@@ -1,6 +1,7 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+
 #include <cmath>
 #include <iostream>
 
