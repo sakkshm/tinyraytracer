46e672b283161a428e7eee58a8557f4b924615c5
sakkshm <saksham.saxena404@gmail.com>
2025-01-22 22:44:46 +0530

    Added positionable camera and Defocus blur

---
+++ tinyraytracer.h
@@ -0,0 +1,12 @@
+//All Headers for main.cpp
+
+#include "headers/color.h"
+#include "headers/vec3.h"
+#include "headers/ray.h"
+#include "headers/rt_weekend.h"
+#include "headers/hittable.h"
+#include "headers/hittable_list.h"
+#include "headers/sphere.h"
+#include "headers/interval.h"
+#include "headers/camera.h"
+#include "headers/material.h"
